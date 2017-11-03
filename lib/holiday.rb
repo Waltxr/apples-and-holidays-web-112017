b1c666fc0f644b8b08617d13064a8bfbf1f9d8dd
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-03 18:28:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize! + ':'
       holiday.each do |name, supplies|
-      puts "  " + name.to_s + ": " + supplies.flatten.to_s
+      puts "  " + name.to_s + ": " + supplies.join(", ")
     end
   end
 end
