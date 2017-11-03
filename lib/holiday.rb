12d07d66b7bb41196044e73021bbb050d9fadc9e
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-03 18:10:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    print season.to_s + ' : ' + holiday.to_s
+    puts season.to_s + ':'
+    puts "  " + holiday.to_s
   end
 
 end
